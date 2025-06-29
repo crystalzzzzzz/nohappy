d5bcacf53a945725cce2ccf0484aab17a4931a67
crystalzzzzzz <160002394+crystalzzzzzz@users.noreply.github.com>
2025-06-30 01:29:58 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { StagewiseToolbar } from '@stagewise/toolbar-vue'
+import { VuePlugin } from '@stagewise-plugins/vue'
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+import Benefits from "./components/Benefits.vue";
+import Features from "./components/Features.vue";
+import Services from "./components/Services.vue";
+import HowItWorks from "./components/HowItWorks.vue";
+import Sponsors from "./components/Sponsors.vue";
+import Report from "./components/Report.vue";
+import Team from "./components/Team.vue";
+import Contact from "./components/Contact.vue";
+import FAQ from "./components/FAQ.vue";
+import Footer from "./components/Footer.vue";
+import QuestionnaireAssessment from "./components/QuestionnaireAssessment.vue";
+import ECGScreening from "./components/ECGScreening.vue";
+import GeneScreening from "./components/GeneScreening.vue";
+import EmotionRecognition from "./components/EmotionRecognition.vue";
+
+const config = {
+  plugins: [VuePlugin],
+}
+</script>
+
+<template>
+  <StagewiseToolbar :config="config" />
+  <Navbar />
+  <Hero />
+  <Sponsors />
+  <Benefits />
+  <Features />
+  <Services />
+  <HowItWorks />
+  <QuestionnaireAssessment />
+  <ECGScreening />
+  <GeneScreening />
+  <EmotionRecognition />
+  <Report />
+  <Team />
+  <Contact />
+  <FAQ />
+  <Footer />
+</template>
